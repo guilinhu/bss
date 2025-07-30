51b329416711921a2d5848b112eaae3d7ce058f8
Guilin Hu <79238697+guilinhu@users.noreply.github.com>
2025-07-30 00:09:10 -0700

    Update MultiScaleLoss.py

--- src/losses/MultiScaleLoss.py
+++ src/losses/MultiScaleLoss.py
@@ -3,17 +3,29 @@ import torch.nn as nn
 import itertools
 
 # from asteroid.losses.sdr import SingleSrcNegSDR
+# from asteroid.losses import pairwise_neg_sisdr
+
 from asteroid.losses.sdr import PairwiseNegSDR
 from asteroid.losses.pit_wrapper import PITLossWrapper
 import torch.nn.functional as F
 
 
+def assert_no_allzero_slices(x: torch.Tensor, dim: int = -1):
+    zero_mask = x.eq(0).all(dim=dim)
+
+    if zero_mask.any():
+        idxs = torch.nonzero(zero_mask, as_tuple=False)
+        raise AssertionError(f"all-zero slices at indices {idxs.tolist()}")
+
+
 class MultiScaleLoss(nn.Module):
-    def __init__(self, loss_type="sisdr", recon=0.9, attr=0.1):
+    def __init__(self, loss_type="sisdr", recon=1.0, attr=1.0, only_last=False):
         super().__init__()
         self.loss_type = loss_type
+        self.only_last = only_last
         # self.sdr_loss = SingleSrcNegSDR(loss_type)  # Supports 'sisdr', 'snr', etc.
         self.pit_wrapper = PITLossWrapper(PairwiseNegSDR(loss_type))
+        # self.pit_wrapper = PITLossWrapper(pairwise_neg_sisdr, pit_from="pw_mtx")
         self.bce = nn.BCEWithLogitsLoss()  # For attractor existence loss
 
         self.recon = recon
@@ -23,24 +35,42 @@ class MultiScaleLoss(nn.Module):
         # print("all estimate {}".format(all_estimates[0].shape))[4, 4, 64000]
         assert all_est_list[0].shape == gt.shape, f"Mismatch: est shape {all_est_list[0].shape}, gt shape {gt.shape}"
 
+        # zero_tensor = torch.zeros_like(gt)
+        assert_no_allzero_slices(gt)
+
         recon_loss = 0.0
         last_reordered = None
-        for est in all_est_list:
-            # print("est {}".format(est.shape))
-            # print("gt {}".format(gt.shape))
-            assert not torch.isnan(est).any() and not torch.isinf(est).any(), "Found nan/inf in est"
-            assert not torch.isnan(gt).any() and not torch.isinf(gt).any(), "Found nan/inf in gt"
-            loss, reordered = self.pit_wrapper(est, gt, return_est=True)
-            loss = torch.clamp(loss, min=-30.0)
+        if not self.only_last:
+            for est in all_est_list:
+                assert_no_allzero_slices(est)
+                assert not torch.isnan(est).any() and not torch.isinf(est).any(), "Found nan/inf in est"
+                assert not torch.isnan(gt).any() and not torch.isinf(gt).any(), "Found nan/inf in gt"
+                loss, reordered = self.pit_wrapper(est, gt, return_est=True)
+                recon_loss += loss
+                last_reordered = reordered
+
+            N = len(all_est_list)
+            # print("N is {}".format(N))
+            recon_loss = recon_loss / N
+            # recon_loss = torch.clamp(recon_loss, max=30.0)
+
+        else:
+            loss, reordered = self.pit_wrapper(all_est_list[-1], gt, return_est=True)
+            # loss = torch.clamp(loss, min=-30.0)
             recon_loss += loss
             last_reordered = reordered
 
-        N = len(all_est_list)
-        # print("N is {}".format(N))
-        recon_loss = recon_loss / N
+        # assert_no_allzero_slices(all_est_list[-1])
+        # loss, reordered = self.pit_wrapper(all_est_list[-1], gt, return_est=True)
+        # loss = torch.clamp(loss, min=-30.0)
+        # recon_loss += loss
+        # last_reordered = reordered
 
         # attractor loss
-        attractors = attractors[:, : n_speakers + 1]
+        # attractors length C+1
+        attr_len = attractors.shape[-1]
+        assert attr_len == n_speakers + 1
+        # attractors = attractors[:, : n_speakers + 1]
         B, C_plus_1 = attractors.shape
         # create [1 ... 1 0] target
         labels = torch.zeros(B, C_plus_1, device=attractors.device)
@@ -52,6 +82,6 @@ class MultiScaleLoss(nn.Module):
         if return_perm:
 
             # return reordered estimates as well
-            return total_loss, last_reordered
+            return total_loss, recon_loss, attractor_loss, last_reordered
 
-        return total_loss
+        return total_loss, recon_loss, attractor_loss

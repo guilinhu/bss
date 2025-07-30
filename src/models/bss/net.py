93ec1c0a0009d1b51848274e014ddbbe31d211d4
Guilin Hu <79238697+guilinhu@users.noreply.github.com>
2025-07-29 23:58:22 -0700

    Update net.py

--- src/models/bss/net.py
+++ src/models/bss/net.py
@@ -16,17 +16,11 @@ class Net(nn.Module):
         self.L = L
         self.in_channel = in_channel
 
-        self.conv1d = nn.Conv1d(
-            in_channels=in_channel,
-            out_channels=De,
-            kernel_size=L,
-            stride=L // 2,
-            padding=L // 2,
-        )
+        self.conv1d = nn.Conv1d(in_channels=in_channel, out_channels=De, kernel_size=L, stride=L // 2, padding=L // 2)
         self.activation = nn.GELU()
 
         self.transconv1d = nn.ConvTranspose1d(
-            in_channels=De, out_channels=1, kernel_size=L, stride=L // 2, padding=L // 2, output_padding=0
+            in_channels=De, out_channels=1, kernel_size=L, stride=L // 2, padding=L // 2, output_padding=0, bias=False
         )
 
         self.separator = Separator(De, D, hidden_dim, K, M, n_head, dropout, N, max_spk)
@@ -49,8 +43,6 @@ class Net(nn.Module):
         # print(x.shape)
         B, c, T = x.shape
 
-        # print(x.shape) [1, 1, 64000]
-
         # encoder
         x = self.conv1d(x)  # [B, De, T']
         x = self.activation(x)
@@ -59,15 +51,13 @@ class Net(nn.Module):
 
         if "num_target_speakers" in inputs:
             C = inputs["num_target_speakers"]
-            # print("C is {}".format(C))
             # === Separator ===
+            # attractor_logits C+1
             sep_out_list, attractor_logits = self.separator(x, C)  # [B, C, T', De]
 
             # unsure
-            B, C, T_, De = sep_out_list[-1].shape
-            # print("c is {}".format(C))
-
-            # print("sep out {}".format(sep_out.shape)) [1, 3, 8064, 256]
+            B, C_new, T_, De = sep_out_list[-1].shape
+            assert C.max().item() == C_new
 
             # decoder
             # decoder expects shape: [B * C, De, T']
@@ -76,38 +66,41 @@ class Net(nn.Module):
                 sep_out = sep_out.permute(0, 1, 3, 2).contiguous()  # [B, C, De, T']
                 sep_out = sep_out.view(B * C, De, T_)
 
-                # print("sep out {}".format(sep_out.shape))[4, 256, 4001]
-
                 waveform = self.transconv1d(sep_out)  # [B * C, 1, T_wav]
                 waveform = waveform.view(B, C, -1)  # [B, C, T_wav]
 
-                # print("wav is {}".format(waveform.shape))[1, 4, 32000]
-
-                waveform = waveform[:, :, :T]
+                # waveform = waveform[:, :, :T]
+                mean = waveform.mean(dim=-1, keepdim=True)
+                waveform = waveform - mean
                 output_list.append(waveform)
-
-            # print("output {}".format(waveform.shape)) [1, 4, 64520]
-            # print("logits {}".format(attractor_logits.shape)) [1, 5]
 
             return {"output": waveform, "attractor_logits": attractor_logits, "output_list": output_list}
 
         else:
             # === Inference ===
+            # attractor_logits length C_max+1
             sep_out_list, attractor_logits = self.separator(x, C=None)  # [B, C_max, T', De]
+            # print(sep_out_list[-1].shape)
 
             probs = self.sigmoid(attractor_logits)  # (B, C_max+1)
+            # print(probs)
 
             probs = probs[:, :-1]
-            estimated_C = (probs > 0.5).sum(dim=1).clamp(min=1)  # (B,) avoid C=0
 
+            estimated_C = (probs > 0.5).sum(dim=1).clamp(min=1)  # (B,) avoid C=0
             # TODO: for now take max estimated speaker from the whole batch
             C = estimated_C.max().item()
             # when C not specified, sep_out contains C_max streams
-            sep_out = sep_out[-1][:, :C, :, :]  # Take top-C speakers across batch
+            sep_out = sep_out_list[-1][:, :C, :, :]  # Take top-C speakers across batch
 
-            B, C, T_, De = sep_out.shape
+            # mask = probs[0] > 0.5
+            # if not mask.any():
+            #     best = probs.argmax(dim=1)[0].item()  # scalar idx
+            #     mask[best] = True
+            # sep_out = sep_out_list[-1][:, mask, :]
 
-            # print("sep out {}".format(sep_out.shape)) [1, 3, 8064, 256]
+            B, C_new, T_, De = sep_out.shape
+            assert C == C_new
 
             # decoder
             # decoder expects shape: [B * C, De, T']
@@ -117,7 +110,9 @@ class Net(nn.Module):
             waveform = self.transconv1d(sep_out)  # [B * C, 1, T_wav]
             waveform = waveform.view(B, C, -1)  # [B, C, T_wav]
 
-            waveform = waveform[:, :, :T]
+            # waveform = waveform[:, :, :T]
+            mean = waveform.mean(dim=-1, keepdim=True)
+            waveform = waveform - mean
 
             return {"output": waveform, "attractor_logits": attractor_logits}
 
